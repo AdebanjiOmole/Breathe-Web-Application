d72d8bb47f76c5e264747ff55e84809bcc7f56d1
Arinpe <dehtolah@gmail.com>
2022-08-12 12:04:42 +0100

    Fixed errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store'
+import { store } from '../src/redux/store'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
