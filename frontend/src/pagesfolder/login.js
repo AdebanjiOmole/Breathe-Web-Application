b5414213f9ed3bc9c57838d2f7c1c0fd7c5efbd8
Arinpe <dehtolah@gmail.com>
2022-08-14 11:16:32 +0100

    Update Web-page

--- frontend/src/pagesfolder/login.js
+++ frontend/src/pagesfolder/login.js
@@ -4,15 +4,15 @@ const login = () => {
   return (
     <div class="container" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/image/maproad.jpg'})` }} >
         <form class="form" id="createAccount">
-            <a class="logosignin" href="index.html"><img src="assets/image/Breathe logo.png" alt="" /></a>
-            <h1 class="form__title">Breathe</h1>
+            <link class="logosignin" href="index.html"><img src="assets/image/Breathe logo.png" alt="" /></link>
+            <h1 class="form__title">Sign In</h1>
             <div class="form_message form_message--error"></div>
             <div class="form__input-group">
-                <input type="text" class="form__input" autofocus placeholder="Email Address" />
-                <div class="form__input-error-message"></div>
+                <input type="text" className="Username" value="Email Address" />
+                <div className="form__input-error-message"></div>
             </div>
-            <div class="form__input-group">
-                <input type="password" class="form__input" autofocus placeholder="Password" />
+            <div className="form__input-group">
+                <input type="password" className="form__input" value={} autofocus placeholder="Password" />
                 <div class="form__input-error-message"></div>
             </div>
             <p class="form__text">
